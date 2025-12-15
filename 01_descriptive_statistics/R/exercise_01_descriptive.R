03f6e57c64d0228ece6a37aaac5000e536185391
Ivan <timing350@gmail.com>
2025-12-15 16:26:16 +0100

    Add Exercise 01 descriptive statistics (student version)

---
+++ 01_descriptive_statistics/R/exercise_01_descriptive.R
@@ -0,0 +1,35 @@
+############################################################
+# Course: Statistical Methods
+# Unit: 01 - Descriptive Statistics
+# Exercise: 01
+#
+# Description:
+# The following dataset corresponds to the expenditure (in euros)
+# of 100 customers in a store.
+#
+# Tasks:
+# a) Compute the mean.
+# b) Compute the median.
+# c) Compute the interquartile range (IQR).
+# d) Compute the standard deviation.
+# e) The coefficient of variation.
+# f) The skewness or coefficient of asymmetry.
+# e) Kurtosis or coefficient of peakedness.
+############################################################
+
+# Write your R code below
+
+expenditures <- c(
+  21.8, 40.8, 25.1, 34.6, 39.1, 37.8, 38.4, 36.0, 34.4, 30.1,
+  25.1, 37.4, 38.0, 25.1, 37.8, 29.4, 34.3, 27.2, 23.8, 29.9,
+  26.8, 36.8, 25.0, 36.1, 24.9, 38.5, 23.8, 36.8, 31.7, 20.6,
+  29.8, 24.3, 36.5, 39.0, 40.9, 27.4, 22.2, 28.2, 31.1, 31.5,
+  38.9, 24.6, 22.3, 39.1, 25.9, 21.6, 24.1, 29.4, 22.1, 28.9,
+  42.4, 38.5, 43.8, 25.9, 26.5, 33.8, 20.8, 37.5, 37.6, 38.4,
+  27.4, 28.5, 31.5, 28.5, 25.1, 31.3, 22.3, 25.3, 22.6, 22.3,
+  20.4, 31.1, 21.2, 28.7, 24.9, 23.2, 26.4, 20.6, 27.8, 26.9,
+  30.2, 20.5, 31.1, 30.0, 26.7, 25.4, 25.1, 30.3, 28.8, 23.6,
+  25.3, 26.6, 23.8, 28.5, 27.0, 23.9, 21.9, 31.9, 26.0, 23.5
+)
+
+
