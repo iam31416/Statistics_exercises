c249d48d9ca006d97a419857fed6c95e10221dd7
iam31416 <iam@uma.es>
2026-01-13 13:25:41 +0100

    ejercicio básico

--- 02_statistical_models/R/exercise_1.R
+++ 02_statistical_models/R/exercise_1.R
@@ -4,7 +4,7 @@
 # The observed data are given by the following pairs \((P, C)\):
 #  $ (1,1),\ (2,1),\ (3,2),\ (4,2),\ (5,4)$
 
-# Write a table with all the data.
+# Write a table with all the data and the Joint relative frequency table.
 
 pressure <- c(1, 2, 3, 4, 5)
 compression <- c(1, 1, 2, 2, 4)
@@ -12,7 +12,7 @@ compression <- c(1, 1, 2, 2, 4)
 df <- data.frame(pressure, compression)
 df
 
-################################################ next exercise
+################################################ Joint relative frequency table
 
 table(pressure, compression)
 prop.table(table(pressure, compression))
