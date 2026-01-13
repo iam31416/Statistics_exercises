bf1957b70ee92a96f073d04053495bb6fd33e6de
iam31416 <iam@uma.es>
2026-01-13 13:17:04 +0100

    Describe brevemente el trabajo realizado

---
+++ 02_statistical_models/R/exercise_1.R
@@ -0,0 +1,20 @@
+# Five experimental samples of an insulating material are tested under different pressure levels. 
+# For each sample, the applied pressure \(P\), measured in \(\mathrm{kg/cm}^2\), and the resulting compression magnitude \(C\), measured in millimeters (mm), are recorded.
+#
+# The observed data are given by the following pairs \((P, C)\):
+#  \[
+#    (1,1),\ (2,1),\ (3,2),\ (4,2),\ (5,4).
+#    \]
+
+# Write a table with all the data.
+
+pressure <- c(1, 2, 3, 4, 5)
+compression <- c(1, 1, 2, 2, 4)
+
+df <- data.frame(pressure, compression)
+df
+
+################################################ next exercise
+
+table(pressure, compression)
+prop.table(table(pressure, compression))
